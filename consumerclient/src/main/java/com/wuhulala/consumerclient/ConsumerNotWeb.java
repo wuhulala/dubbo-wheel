fe451bb84b4dd451a32734829aca8889fd960a25
xueaohui <xueaohui_com@163.com>
2017-04-08 15:06:31 +0800

    测试zookeeper宕机

--- consumerclient/src/main/java/com/wuhulala/consumerclient/ConsumerNotWeb.java
+++ consumerclient/src/main/java/com/wuhulala/consumerclient/ConsumerNotWeb.java
@@ -15,7 +15,7 @@ import java.io.IOException;
  */
 public class ConsumerNotWeb {
     public static final Logger logger = LoggerFactory.getLogger(ConsumerNotWeb.class);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[] { "consumer.xml" });
         context.start();
@@ -23,6 +23,13 @@ public class ConsumerNotWeb {
 //        for (int i = 0; i < 5; i++) {
             new Thread(new ConsumerRunable(testService)).start();
 //        }
+
+//        在睡眠过程中 关掉zookeeper  仍能请求到
+//        注册中心通过长连接感知服务提供者的存在，服务提供者宕机，注册中心将立即推送事件通知消费者
+//        注册中心和监控中心全部宕机，不影响已运行的提供者和消费者，消费者在本地缓存了提供者列表
+        Thread.sleep(10000);
+        new Thread(new ConsumerRunable(testService)).start();
+
         try {
             System.in.read();
         } catch (IOException e) {
